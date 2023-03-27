e85d02b65ff2fc0810c2015fd37c8b99ee1e4ded
Ishy720 <76483561+Ishy720@users.noreply.github.com>
2023-03-27 16:15:44 +0100

    Commented schemas

--- application/backend/models/BookSchema.js
+++ application/backend/models/BookSchema.js
@@ -1,3 +1,7 @@
+/*
+    This is the schema for my books in my MongoDB database.
+*/
+
 const { Schema, model } = require('mongoose');
 
 const BookSchema = new Schema({
--- application/backend/models/UserSchema.js
+++ application/backend/models/UserSchema.js
@@ -1,3 +1,7 @@
+/*
+    This is the schema for my users in my MongoDB database.
+*/
+
 const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema(
